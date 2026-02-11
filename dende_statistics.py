e7774fc167b12eac99f979ded7a4f4c7725a7cfe
João Mira <jmirauniversitario@gmail.com>
2026-02-10 22:03:48 -0300

    Funções Probabilidade Condicional, Quartis e Histograma finalizadas

--- dende_statistics.py
+++ dende_statistics.py
@@ -217,6 +217,27 @@ class Statistics:
         float
             A probabilidade condicional, um valor entre 0 e 1.
         """
+        # Recebe os valores do dataset
+        values = self.dataset[column]
+
+        # Contadores para a fórmula
+        total_b = 0  # Quantas vezes o valor condicionante aparece (divisor)
+        sucessos_ba = 0  # Quantas vezes a sequência (B -> A) ocorre (numerador)
+
+        # Percorre até o penúltimo elemento
+        for i in range(len(values) - 1):
+            # Verifica se o elemento atual é o condicionante (B)
+            if values[i] == value2:
+                total_b += 1
+
+                # Verifica se o consequente (A) aparece imediatamente após o condicionante (B)
+                if values[i + 1] == value1:
+                    sucessos_ba += 1
+        # Proteção contra divisão por zero (caso value2 não exista ou seja o último)
+        if total_b == 0:
+            return 0.0
+
+        return sucessos_ba / total_b
         pass
 
     def quartiles(self, column):
@@ -233,6 +254,46 @@ class Statistics:
         dict
             Um dicionário com os quartis Q1, Q2 (mediana) e Q3.
         """
+
+        # Recebendo os valores do dataset
+        values = sorted(self.dataset[column])
+        n = len(values)
+
+        # Caso a quantidade de valores for igual a zero
+        if n == 0:
+            return {"Q1": 0, "Q2": 0, "Q3": 0}
+
+        # Cálculo do Q2 (Mediana)
+        mid = n // 2
+        if n % 2 == 0:
+            q2 = (values[mid - 1] + values[mid]) / 2
+            # O fatiamento começa do início até o mid
+            lower_half = values[:mid]
+            # O fatiamento começa do mid até o final
+            upper_half = values[mid:]
+        else:
+            q2 = values[mid]
+            # Para n ímpar, a mediana (Q2) é excluída de ambas as metades
+            lower_half = values[:mid]
+            upper_half = values[mid + 1:]
+
+        # Cálculo do Q1 (Mediana da Metade Inferior)
+        n_lower = len(lower_half)
+        mid_l = n_lower // 2
+        if n_lower % 2 == 0:
+            q1 = (lower_half[mid_l - 1] + lower_half[mid_l]) / 2
+        else:
+            q1 = (lower_half[mid_l])
+
+        # Cálculo do Q3 (Mediana da Metade Superior
+        n_upper = len(upper_half)
+        mid_u = n_upper // 2
+        if n_upper % 2 == 0:
+            q3 = (upper_half[mid_u - 1] + upper_half[mid_u]) / 2
+        else:
+            q3 = (upper_half[mid_u])
+
+        return {"Q1": q1, "Q2": q2, "Q3": q3}
         pass
 
     def histogram(self, column, bins):
@@ -252,5 +313,37 @@ class Statistics:
             Um dicionário onde as chaves são os intervalos (tuplas)
             e os valores são as contagens.
         """
-        pass
 
+        valores = self.dataset[column]
+        menor_valor,  valor_maior = min(valores), max(valores)
+        numero_bins = 4
+        tamanho_bin = (valor_maior - menor_valor) / numero_bins
+
+        # Criação os limites dos intervalos (os buckets)
+        limites = []
+        for i in range(numero_bins + 1):
+            ponto = menor_valor + (i * tamanho_bin)
+            limites.append(ponto)
+
+        # Geração do dicionário com tuplas como chaves (Início, Fim)
+        # Ex: {(20.0, 35.0): 0, (35.0, 50.0): 0, ...}
+        histograma = {}
+        for i in range(numero_bins):
+            intervalo = (limites[i], limites[i + 1])
+            histograma[intervalo] = 0
+
+        # Realização da contagem
+        for valor in valores:
+            indice = int((valor - menor_valor) / tamanho_bin)
+
+            # Ajuste para o valor máximo
+            if indice == numero_bins:
+                indice -= 1
+
+            # Recupera a chave (tupla) correspondente ao índice para incrementar (Ex: (20.0, 35.0))
+            chave_intervalo = (limites[indice], limites[indice + 1])
+            histograma[chave_intervalo] += 1
+
+        return histograma
+
+        pass
